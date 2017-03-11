a90408e893beffec0da716c47d6b291d6c4bfab0
yuji takahashi <jit.y.31@gmail.com>
2017-03-12 00:07:31 +0900

    :hatched_chick: Init

---
+++ lib/weatherbot.ex
@@ -0,0 +1,18 @@
+defmodule Weatherbot do
+  @moduledoc """
+  Documentation for Weatherbot.
+  """
+
+  @doc """
+  Hello world.
+
+  ## Examples
+
+      iex> Weatherbot.hello
+      :world
+
+  """
+  def hello do
+    :world
+  end
+end
